73687f36d72d8e81a39064716fb63d389fdab305
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-08-09 17:22:46 +0200

    feat: complete build setup w/ nix

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # ZMK Custom Layouts
 
-This repository contains the ZMK user configuration for all my keyboards that use the ZMK firmware.
+This is my personal [ZMK firmware](https://github.com/zmkfirmware/zmk/) configuration for my [sofle](https://github.com/josefadamcik/SofleKeyboard) keyboard.
 
 # Layouts
 
 ![Current layout](./layout/eyelash_sofle.svg)
+
+# Getting started
+
+Currently the only way to build the firmware needed is through nix using the provided `firmware` package
+
+```nix
+nix build .#firmware
+```
+
+This will generate two `uf2` (`zmk_left.uf2` & `zmk_right.uf2`) that needs to be copied on the left & right side of the keyboard
+
+## Building through github action (Currently disabled)
+
+Place the content of this snippet into `.github/workflows/build.yml`
+
+```yaml
+name: Build ZMK firmware
+on:
+  workflow_dispatch:
+  push:
+    paths-ignore:
+      - "layout/**"
+jobs:
+  build:
+    uses: zmkfirmware/zmk/.github/workflows/build-user-config.yml@main
+```
--- flake.nix
+++ flake.nix
@@ -19,37 +19,17 @@
 
     in
     {
-      packages = forAllSystems (
-        system:
-        let
-          src = nixpkgs.lib.sourceFilesBySuffices self [
-            ".board"
-            ".cmake"
-            ".conf"
-            ".defconfig"
-            ".dts"
-            ".dtsi"
-            ".json"
-            ".keymap"
-            ".overlay"
-            ".shield"
-            ".yml"
-            "_defconfig"
-            ".h"
-          ];
-        in
-        {
-          firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-            name = "eyelash_sofle";
-            inherit src;
-            board = "eyelash_sofle_%PART%";
-            shield = "nice_view";
-            zephyrDepsHash = "sha256-IkUO29jGSRcIiV5wAlqC9Xae041yuFg1lYEI+sAX8RM=";
-          };
+      packages = forAllSystems (system: {
+        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+          name = "eyelash_sofle";
+          src = ./.;
+          board = "eyelash_sofle_%PART%";
+          shield = "nice_view_crystal";
+          zephyrDepsHash = "sha256-IkUO29jGSRcIiV5wAlqC9Xae041yuFg1lYEI+sAX8RM=";
+        };
 
-          update = zmk-nix.packages.${system}.update;
-        }
-      );
+        update = zmk-nix.packages.${system}.update;
+      });
 
       devShells = forAllSystems (system: {
         default = zmk-nix.devShells.${system}.default;
