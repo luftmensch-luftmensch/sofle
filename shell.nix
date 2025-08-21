95cb4421b671ff5f2a351858ca9e6e3390e7079a
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-08-21 14:31:41 +0200

    feat: customized nix shell env

--- shell.nix
+++ shell.nix
@@ -18,11 +18,21 @@
       keymap-drawer
     ];
 
-    shellHook = ''
-        printf "\033[1;33m# Welcome to the Sofle CLI environment flake\033[0m\n
-      This flake offers a dev shell with \033[36mkeymap-drawer\033[0m\nthe utility in the PATH\n
-      As a memo here it is the command to regenerate the svg image from the yaml file present in the layout folder\n
-          \033[36mkeymap draw ./layout/eyelash_sofle.yaml > ./layout/eyelash_sofle.svg\033[0m\n"
-    '';
+    shellHook =
+      let
+        file = "./layout/eyelash_sofle";
+        append = path: ext: "${toString path}.${ext}";
+        svg = append file "svg";
+        yaml = append file "yaml";
+        png = append file "png";
+      in
+      ''
+          printf "\033[1;33m# Welcome to the Sofle CLI environment flake\033[0m\n
+        This flake offers a dev shell with \033[36mkeymap-drawer\033[0m\nthe utility in the PATH\n
+        As a memo here it is the command to regenerate the svg image from the yaml file present in the layout folder\n
+            \033[36mkeymap draw ${yaml} > ${svg}\033[0m\n"
+          alias draw="${pkgs.lib.getExe pkgs.keymap-drawer} draw ${yaml} > ${svg}"
+          alias convert-to-png="${pkgs.lib.getExe pkgs.inkscape} --export-type png --export-filename ${png} --export-dpi 300 --export-background=white ${svg}"
+      '';
   };
 }
