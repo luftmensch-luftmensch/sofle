7af584492831e0767c8fa2c1ea97650b8fecaed5
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-09-07 17:45:12 +0200

    Updates:
    * Customized combos: added backspace combo;
    * Layout update

--- shell.nix
+++ shell.nix
@@ -20,19 +20,19 @@
 
     shellHook =
       let
-        file = "./layout/eyelash_sofle";
-        append = path: ext: "${toString path}.${ext}";
-        svg = append file "svg";
-        yaml = append file "yaml";
-        png = append file "png";
+        config = "./layout/config.yaml";
+        source = "./config/eyelash_sofle.keymap";
+        layout = "./layout/layout.yaml";
+        dest = "./layout/layout.svg";
+        keymap = "${pkgs.lib.getExe pkgs.keymap-drawer}";
       in
       ''
-          printf "\033[1;33m# Welcome to the Sofle CLI environment flake\033[0m\n
+        printf "\033[1;33m# Welcome to the Sofle CLI environment flake\033[0m\n
         This flake offers a dev shell with \033[36mkeymap-drawer\033[0m\nthe utility in the PATH\n
         As a memo here it is the command to regenerate the svg image from the yaml file present in the layout folder\n
-            \033[36mkeymap draw ${yaml} > ${svg}\033[0m\n"
-          alias draw="${pkgs.lib.getExe pkgs.keymap-drawer} draw ${yaml} > ${svg}"
-          alias convert-to-png="${pkgs.lib.getExe pkgs.inkscape} --export-type png --export-filename ${png} --export-dpi 300 --export-background=white ${svg}"
+            \033[36mkeymap -c ${config} parse -c 30 -z ${source} -o ${layout} && keymap -c ${config} draw ${layout} -o ${dest} \033[0m\n"
+
+        alias draw="${keymap} -c ${config} parse -c 30 -z ${source} -o ${layout} && ${keymap} -c ${config} draw ${layout} -o ${dest}"
       '';
   };
 }
